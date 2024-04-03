7e86437d6f63791a0ff6653fa531774eb507f4c4
Masashi Okita <okita_masashi@uniquevision.co.jp>
2024-04-03 10:24:13 +0900

    project setting

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,8 @@ import * as vscode from "vscode";
 import { insert, customInsert } from "./insert";
 
 export function activate(context: vscode.ExtensionContext) {
-  let simple = vscode.commands.registerCommand("number.simple-insert", () =>
-    insert()
-  );
-
-  const custom = vscode.commands.registerCommand(
-    "number.custom-insert",
-    async () => await customInsert()
-  );
+  const simple = vscode.commands.registerCommand("number.simple-insert", insert);
+  const custom = vscode.commands.registerCommand("number.custom-insert", customInsert);
 
   context.subscriptions.push(simple);
   context.subscriptions.push(custom);
